fd25642c806c49da9333555b56b2f67cb3f7e6df
mattfwilson2 <mattfwilson@gmail.com>
2022-01-17 18:35:19 -0500

    Test again

--- blackjack_classes3.py
+++ blackjack_classes3.py
@@ -1,7 +1,8 @@
 # how would I calculate the total of the single first card for dealer before they hit (and show their second)
 # says I beat the house even though my total is lower than house
 # aces on house hit are being calculated as 11
-# Test after conflict merge
+# TEST AGAIN
+
 
 import time
 import random
