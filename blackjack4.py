0720b3167fd2e00062707846ffff0775fa3f3d25
mattfwilson <mattfwilson@gmail.com>
2023-01-25 03:30:46 -0500

    Creating function to show piles, show hand, and draw cards

--- blackjack4.py
+++ blackjack4.py
@@ -5,32 +5,40 @@ import random
 count = 0
 deck = deque()
 discard = deque()
+player_hand = []
 
 def buildDeck():
-    suit = ["Spades", "Hearts", "Clubs", "Diamonds"]
-    value = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+    suit = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+    value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
     cards = list(itertools.product(value, suit))
     for i in cards:
         deck.append(i)
     random.shuffle(deck)
     return deck
 
-deck = buildDeck()
+def draw():
+    hand_count = 2
+    for num in range(hand_count):
+        card = deck.popleft()
+        player_hand.append(card)
+    return player_hand
 
-for card in deck:
-    print(card)
-    count += 1
+def showHand():
+    print(f'Draw pile: {len(deck)}')
+    print(f'Discard pile: {len(discard)}')
+
+def showPiles():
+    print(f'Draw pile: {len(deck)}')
+    print(f'Discard pile: {len(discard)}')
+
+deck = buildDeck()
 
 while True:
     draw = input('Draw? ')
     if draw == 'y':
-        card = deck.popleft()
-        discard.append(card)
-        print(f'You drew a {card}')
-        print(f'Draw pile: {len(deck)}')
-        print(f'Discard pile: {len(discard)}')
-    
+        draw()
+        showHand()
+    elif draw == 'piles':
+        showPiles()
     else:
-        print(f'Draw pile: {len(deck)}')
-        print(f'Discard pile: {len(discard)}')
         quit()
