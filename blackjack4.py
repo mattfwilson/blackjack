22771ed222061ee201b0975b116c12f71f2fd0c8
mattfwilson <mattfwilson@gmail.com>
2023-01-25 03:08:37 -0500

    Added discard pile and draw/discard pile counts

--- blackjack4.py
+++ blackjack4.py
@@ -1,13 +1,15 @@
+from collections import deque
 import itertools
 import random
 
 count = 0
+deck = deque()
+discard = deque()
 
 def buildDeck():
     suit = ["Spades", "Hearts", "Clubs", "Diamonds"]
     value = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
     cards = list(itertools.product(value, suit))
-    deck = []
     for i in cards:
         deck.append(i)
     random.shuffle(deck)
@@ -15,9 +17,20 @@ def buildDeck():
 
 deck = buildDeck()
 
-
 for card in deck:
     print(card)
     count += 1
 
-print(f'Deck count: {count}')
+while True:
+    draw = input('Draw? ')
+    if draw == 'y':
+        card = deck.popleft()
+        discard.append(card)
+        print(f'You drew a {card}')
+        print(f'Draw pile: {len(deck)}')
+        print(f'Discard pile: {len(discard)}')
+    
+    else:
+        print(f'Draw pile: {len(deck)}')
+        print(f'Discard pile: {len(discard)}')
+        quit()
