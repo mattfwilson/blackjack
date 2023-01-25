b06d90da1393d7d9aad559304f2a37ced8d0fef8
mattfwilson <mattfwilson@gmail.com>
2023-01-25 00:24:47 -0500

    Added shuffling into the buildDeck func

--- blackjack4.py
+++ blackjack4.py
@@ -1,14 +1,23 @@
 import itertools
 import random
 
+count = 0
+
 def buildDeck():
     suit = ["Spades", "Hearts", "Clubs", "Diamonds"]
     value = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
     cards = list(itertools.product(value, suit))
-    return cards
+    deck = []
+    for i in cards:
+        deck.append(i)
+    random.shuffle(deck)
+    return deck
 
 deck = buildDeck()
-for i in deck:
-    print(i)
 
-# Testing for git
+
+for card in deck:
+    print(card)
+    count += 1
+
+print(f'Deck count: {count}')
